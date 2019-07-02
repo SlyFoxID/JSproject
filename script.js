552c3a86b809cf89c097a92accd4d7c902bea976
Sly_Fox <example@gmail.com>
2019-07-02 21:58:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/*let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+//console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+//console.log(arr[0]);
+
+//alert("Hello World!");
+
+let answer = +prompt("Есть ли вам 18?", "Да");
+
+console.log(answer);
+
+console.log(typeof(answer));
+
+console.log("arr" + " - object");
+
+console.log(4 + " - ")*/
+
+let a = 5;
+
+console.log(a);
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+//console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
